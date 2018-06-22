d0d043d576ab0ba0cf4ddece3fc6b718f4ee213b
Ryan Rosello <ryan.rosello@byojet.com>
2018-06-22 11:52:21 +1000

    completed unit tests for extension methods

--- Jetmax.Testing.Gui/UnitTests/TestPage.cs
+++ Jetmax.Testing.Gui/UnitTests/TestPage.cs
@@ -4,13 +4,14 @@
     {
         public static string EnterTextField = "//*[@type='text']";
         public static string DropDown = "[name='combo-box']";
-        public static string HeadingText = "h1";
         public static string OkButton = "input[type='button']";
         public static string SubscribeCheckbox = "subscribeNews";
-        public static string SubscribeButton = "//button[text()='Subscribe']";
+        public static string DivInsideiFrame = ".inside-frame";
         public static string WikiPeadiaIFrame = "iframe[title*='Avocados']";
-        public static string MainContentArea = ".main-content";
+        public static string DisableField = "#disable-field";
+        public static string HideButton = "#hide";
         public static string ListOption = "//li[contains(text(), 'RUNTIME')]";
-        public static string LinkToStackOverflow = "#a-link";
+        public static string HiddenUnclickable = "#hidden-unclickable";
+        public static string SecretText = "#toggle-visibility";
     }
 }
--- Jetmax.Testing.Gui/UnitTests/Unit.cs
+++ Jetmax.Testing.Gui/UnitTests/Unit.cs
@@ -21,28 +21,27 @@ namespace Jetmax.Testing.Gui.UnitTests
             string codeBase = Assembly.GetExecutingAssembly().Location;
             var dir = new FileInfo(codeBase).Directory + @"\UnitTests\Test.html";
             _baseUrl = new Uri(dir).ToString();
+            Wd.Visit(_baseUrl);
         }
 
         [TestMethod]
         public void TestInputtingValueIntoTextField()
         {
-            Console.WriteLine("Test");
-            Wd.Visit(_baseUrl);
             Wd.Get(TestPage.EnterTextField).SetText("cheese");
             Assert.IsTrue(Wd.Get(TestPage.EnterTextField).GetAttribute("value") == "cheese");
         }
 
         [TestMethod]
+        [ExpectedException(typeof(OpenQA.Selenium.NoSuchElementException),
+            "Unable to locate element")]
         public void TestNonExistentElement()
         {
-            Wd.Visit(_baseUrl);
             Wd.Get("#this-does-not-exists");
         }
 
         [TestMethod]
         public void TestElementRequiringRuntimeData()
         {
-            Wd.Visit(_baseUrl);
             var textContent = Wd.Get(TestPage.ListOption, "Coffee").Text;
             Assert.AreEqual(textContent, "Black Coffee");
         }
@@ -50,37 +49,49 @@ namespace Jetmax.Testing.Gui.UnitTests
         [TestMethod]
         public void TestClickingElement()
         {
-
+            Wd.Get(TestPage.HideButton).PerformClick();
+            Assert.IsFalse(Wd.Get(TestPage.SecretText).Displayed);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(System.Exception),
+            "Element was not visible and enabled")]
         public void TestForElementUnclikable()
         {
-
+            Wd.Get(TestPage.HiddenUnclickable).PerformClick();
         }
 
         [TestMethod]
+        [ExpectedException(typeof(OpenQA.Selenium.NoSuchElementException),
+            "Unable to locate element")]
         public void TestInputElementUneditable()
         {
-
+            Wd.Get(TestPage.DisableField).SetText("THis field should fail input because it disabled");
         }
 
         [TestMethod]
         public void TestSelectValueFromDropDown()
         {
-
+            Wd.Get(TestPage.DropDown).SelectOption("Third Value");
+            Assert.AreEqual("third", Wd.Get(TestPage.DropDown).GetAttribute("value"));
         }
 
         [TestMethod]
         public void TestTickingCheckbox()
         {
-
+            Wd.Get(TestPage.SubscribeCheckbox).PerformClick();
+            Assert.IsTrue(Wd.Get(TestPage.SubscribeCheckbox).Selected);
         }
 
         [TestMethod]
         public void TestSwitchingBetweenIframe()
         {
-
+            Wd.Get(TestPage.OkButton).PerformClick();
+            Wd.SwitchIframe(TestPage.WikiPeadiaIFrame);
+            var textInsideIframe = Wd.Get(TestPage.DivInsideiFrame).Text;
+            Assert.AreEqual("This is insed the iFrame", textInsideIframe);
+            Wd.SwitchBackToMainContent();
+            Wd.Get(TestPage.OkButton).PerformClick();
         }
 
         [TestCleanup]
