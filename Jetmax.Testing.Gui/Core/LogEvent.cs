01b4b76b50e95c117844c7204560693b4dd7c7b5
Ryan Rosello <ryan.rosello@byojet.com>
2018-06-22 12:50:13 +1000

    implemented logger

---
+++ Jetmax.Testing.Gui/Core/LogEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Jetmax.Testing.Gui.Core
+{
+    public class LogEvent
+    {
+        public DateTime EventDate { get; set; }
+        public string Classification { get; set; }
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return $"{EventDate:s} {Classification} {Message}";
+        }
+    }
+}
